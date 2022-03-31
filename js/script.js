fbaa8cd79a6a2e0ca1a6a5fe68f9f98ac4c67fc7
Elisa Solazzo <elisa.solazzo@gmail.com>
2022-03-31 21:01:31 +0200

    first import completed
    imported YasirKareem's gradient bg work

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*global $*/
+
+$(function () {
+    'use strict';
+    var view = $(window).height();
+    $('.part')
+        .height(view)
+        .scrollie({
+            scrollOffset: -50,
+            scrollingInView: function (elem) {
+                var bgColor = elem.data('background');
+                $('body').css('background-color', bgColor);
+            }
+        });
+});
